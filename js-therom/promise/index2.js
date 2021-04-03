602b49d73bb693510defde850978884f816c73d0
po12359 <79793382+po12359@users.noreply.github.com>
2021-04-03 22:06:53 +0900

    Update index2.js

--- js-therom/promise/index2.js
+++ js-therom/promise/index2.js
@@ -46,3 +46,124 @@ user1
   .then((num) => {
     console.log(num);
   });
+
+// class UserPrint {
+//   loginUser(id, password, onSuessecc, onError) {
+//     if (id === `ellie` && password === `123`) {
+//       onSuessecc(id);
+//     } else {
+//       onError(new Error(`get out here`));
+//     }
+//   }
+//   getUser(user, onSuessecc, onError) {
+//     if (user === `ellie`) {
+//       onSuessecc({ user: `ellie`, role: `admin` });
+//     } else {
+//       onError(new Error(`get out here`));
+//     }
+//   }
+// }
+
+// const id = prompt(`enter your id`);
+// const password = prompt(`enter your password`);
+
+// const userPrint = new UserPrint();
+// userPrint.loginUser(
+//   id,
+//   password,
+//   (user) => {
+//     userPrint.getUser(
+//       user,
+//       (userInfo) => {
+//         alert(`user : ${userInfo.user}, role : ${userInfo.role}`);
+//       },
+//       (error) => {
+//         console.log(error);
+//       }
+//     );
+//   },
+//   (error) => console.log(error)
+// );
+
+{
+  let result = new Promise(function numver(resolve, reject) {
+    setTimeout(() => resolve(1), 1000);
+  });
+  result
+    .then((num) => num + 1)
+    .then((num) => num * 3)
+    .then((num) => {
+      console.log(num);
+    });
+}
+
+// const getHen = () => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => resolve(`🐓`), 2000);
+//   });
+// };
+// const getEgg = (hen) =>
+//   new Promise((resolve, reject) => {
+//     setTimeout(() => resolve(`${hen} => 🥚`), 1000);
+//   });
+
+// const cook = (egg) =>
+//   new Promise((resolve, reject) => {
+//     setTimeout(() => resolve(`${egg} => 🍗`), 1000);
+//   });
+
+// getHen()
+//   .then((hen) => {
+//     return getEgg(hen);
+//   })
+//   .then((egg) => {
+//     return cook(egg);
+//   })
+//   .then((result) => {
+//     console.log(result);
+//   });
+
+const sex = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error(`get out here`));
+  }, 1000);
+});
+
+sex.catch((error) => console.log(error));
+
+const getHen = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve(`🐓`), 1000);
+    // setTimeout(() => reject(new Error(`error! 🐓`)), 1000);
+  });
+};
+const getEgg = (hen) =>
+  new Promise((resolve, reject) => {
+    // setTimeout(() => resolve(`${hen} => 🥚`), 1000);
+    setTimeout(() => reject(new Error(`error! ${hen} => 🥚`)), 1000);
+  });
+const cook = (egg) =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => resolve(`${egg} => 🍗`), 1000);
+    // setTimeout(() => reject(new Error(`error! ${egg} => 🍗`)), 1000);
+  });
+
+getHen()
+  .catch((error) => {
+    console.log(error);
+    return `💀`; // 빵꾸처리
+  })
+  .then((hen) => getEgg(hen))
+  .catch((error) => {
+    console.log(error);
+    return `💀`; // 빵꾸처리
+  })
+  .then((egg) => cook(egg))
+  .catch((error) => {
+    console.log(error);
+    return `💀`; // 빵꾸처리
+  })
+  .then((result) => console.log(result))
+  .catch((error) => {
+    console.log(error);
+  });
