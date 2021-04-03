ff66d8717116886aa7f30325a30fc283efb85b83
po12359 <79793382+po12359@users.noreply.github.com>
2021-04-03 22:06:11 +0900

    arrow up

--- dreamcoding/index.js
+++ dreamcoding/index.js
@@ -1,51 +1,4 @@
 `use strict`;
-// const navbar = document.querySelector(`#navbar`);
-// const navbarHeight = navbar.getBoundingClientRect().height;
-// const home = document.querySelector(`.home__container`);
-// const homeHeight = home.getBoundingClientRect().height;
-// document.addEventListener(`scroll`, () => {
-//   console.log(window.scrollY);
-//   console.log(`navbarHeight: ${navbarHeight}`);
-//   if (window.scrollY > navbarHeight) {
-//     navbar.classList.add(`navbar--dark`);
-//   } else {
-//     navbar.classList.remove(`navbar--dark`);
-//   }
-//   home.style.opacity = 1 - window.scrollY / homeHeight;
-// });
-// //widow.scrooly 현재 스크롤의 위치를 알려주는
-// //getboundingclientrect : 보여지는 화면의 크기 ,
-// // offsegwidth : 속성값으로 저장된 크기
-
-// // handle scrolling when tapping on the navbar menu
-// const navbarMenu = document.querySelector(`.navbar__menu`);
-// navbarMenu.addEventListener(`click`, (event) => {
-//   const target = event.target;
-//   const link = target.dataset.link;
-//   if (link == null) {
-//     return;
-//   }
-//   console.log(event.target.dataset.link);
-//   const scrollTo = document.querySelector(link);
-//   scrollTo.scrollIntoView({ behavior: `smooth` });
-// });
-
-// const contact = document.querySelector(`.home__contact`);
-// contact.addEventListener(`click`, (event) => {
-//   const target = event.target; //home__contact
-//   const link = target.dataset.link; //contact
-//   if (link == null) {
-//     return;
-//   }
-//   console.log(event.target.dataset.link);
-//   const scrollTo = document.querySelector(link);
-//   scrollTo.scrollIntoView({ behavior: `smooth` });
-// });
-
-// document.addEventListener(`scroll`, () => {
-//   console.log(window.scrollY);
-
-// });
 
 const navbar = document.querySelector(`#navbar`);
 const navbarHeight = navbar.getBoundingClientRect().height;
@@ -74,17 +27,49 @@ navbar.addEventListener(`click`, (event) => {
   console.log(event.target.dataset.link);
   const scrollTo = document.querySelector(link);
   scrollTo.scrollIntoView({ behavior: `smooth` });
+  scrollIntoView(link);
 });
-
+//make home slowly fade to transparent as the window scrolls down
 const homeContact = document.querySelector(`.home__contacat`);
 
-home.addEventListener(`click`, (event) => {
-  const target = event.target;
-  const link = target.dataset.link;
-  if (link == null) {
-    return;
-  }
-  console.log(event.target.dataset.link);
-  const scrollTo = document.querySelector(link);
-  scrollTo.scrollIntoView({ behavior: `smooth` });
+home.addEventListener(`click`, () => {
+  // const target = event.target;
+  // const link = target.dataset.link;
+  // if (link == null) {
+  //   return;
+  // }
+  // console.log(event.target.dataset.link);
+  // const scrollTo = document.querySelector(link);
+  // scrollTo.scrollIntoView({ behavior: `smooth` });
+  scrollIntoView(`#contact`);
 });
+
+//show "arrow up" button when scolling down
+const arrowUp = document.querySelector(`.arrow-up, .fas fa-arrow-up`);
+const arrowUpHeight = arrowUp.getBoundingClientRect().height;
+
+arrowUp.addEventListener(`click`, () => {
+  // const target = event.target;
+  // const link = target.dataset.link;
+  // if (link == null) {
+  //   return;
+  // }
+  // console.log(event.target.dataset.link);
+  // const scrollTo = document.querySelector(link);
+  // scrollTo.scrollIntoView({ behavior: `smooth` });
+  scrollIntoView(`#home`);
+});
+
+document.addEventListener(`scroll`, () => {
+  console.log(arrowUpHeight);
+  if (window.scrollY > homeHeight / 2) {
+    arrowUp.classList.add(`arrow-up-dark`);
+  } else {
+    arrowUp.classList.remove(`arrow-up-dark`);
+  }
+});
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: `smooth` });
+}
