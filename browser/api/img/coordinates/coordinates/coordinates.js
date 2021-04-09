53baa0cbf95c80cb41c807fce38f0035d317d37c
po12359 <79793382+po12359@users.noreply.github.com>
2021-04-10 03:55:26 +0900

    coordinates-clean

--- browser/api/img/coordinates/coordinates/coordinates.js
+++ browser/api/img/coordinates/coordinates/coordinates.js
@@ -1,38 +1,28 @@
-/*
 const horizontal = document.querySelector(`.horizontal`);
 const vertical = document.querySelector(`.vertical`);
 const target = document.querySelector(`.target`);
 const tag = document.querySelector(`.tag`);
+addEventListener(`load`, () => {
+  const targetRect = target.getBoundingClientRect();
+  const targetHalfHeight = targetRect.height / 2;
+  const targetHalfWidth = targetRect.width / 2;
+  document.addEventListener(`mousemove`, (event) => {
+    const x = `${event.clientX}`;
+    const y = `${event.clientY}`;
+    horizontal.style.transform = `translateY(${y}px)`;
+    vertical.style.transform = `translateX(${x}px)`;
+    target.style.transform = `translate(${x - targetHalfWidth}px,${
+      y - targetHalfHeight
+    }px)`;
+    // horizontal.style.top = `${y}px`;
+    // vertical.style.left = `${x}px`;
+    // target.style.top = `${y}px`;
+    // target.style.left = `${x}px`;
+    // tag.style.top = `${y}px`;
+    // tag.style.left = `${x}px`;
 
-document.addEventListener(`mousemove`, (event) => {
-  const x = `${event.clientX}`;
-  const y = `${event.clientY}`;
+    tag.innerHTML = `${x}px+${y}px`;
 
-  horizontal.style.top = `${y}px`;
-  vertical.style.left = `${x}px`;
-  target.style.left = `${x}px`;
-  target.style.top = `${y}px`;
-  tag.style.left = `${x}px`;
-  tag.style.top = `${y}px`;
-
-  tag.innerHTML = `${x}px, ${y}px`;
-});
-*/
-const horizontal = document.querySelector(`.horizontal`);
-const vertical = document.querySelector(`.vertical`);
-const target = document.querySelector(`.target`);
-const tag = document.querySelector(`.tag`);
-
-document.addEventListener(`mousemove`, (event) => {
-  const x = `${event.clientX}`;
-  const y = `${event.clientY}`;
-
-  horizontal.style.top = `${y}px`;
-  vertical.style.left = `${x}px`;
-  target.style.left = `${x}px`;
-  target.style.top = `${y}px`;
-  tag.style.left = `${x}px`;
-  tag.style.top = `${y}px`;
-
-  tag.innerHTML = `${x}px,${y}px`;
+    tag.style.transform = `translate(${x}px,${y}px)`;
+  });
 });
